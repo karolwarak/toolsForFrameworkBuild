456502438ddc44fba2574f41cb16df2da8fec508
Karol Warakomski <k.warakomski1@gmail.com>
2019-02-07 23:14:56 +0100

    first test with testNG annotation

---
+++ src/_1_BasicScript.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class _1_BasicScript {
+
+    @Test
+    public void firstTestInTestNg(){
+        System.out.println("firs test with testNG annotation");
+    }
+}
