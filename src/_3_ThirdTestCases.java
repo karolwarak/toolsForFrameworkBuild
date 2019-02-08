cab76741736139dbc7f3db8068201bd28296f0b9
Karol Warakomski <k.warakomski1@gmail.com>
2019-02-08 06:18:07 +0100

    added 2 more classes

---
+++ src/_3_ThirdTestCases.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.Test;
+
+public class _3_ThirdTestCases {
+
+    @Test
+    public void WebLoginCarLoan(){
+        // selenium code
+        System.out.println("Web login car ");
+    }
+
+    @Test
+    public void MobileLoginCarLoan(){
+        System.out.println("Mobile login car ");
+
+
+    }
+
+    @Test
+    public void LoginApiCarLoan(){
+        // rest API automation
+        System.out.println("API login car ");
+
+    }
+
+
+}
---
+++ src/_4_FourthTestCases.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+public class _4_FourthTestCases {
+
+    @Test
+    public void WebLoginHomeLoan(){
+        // selenium code
+        System.out.println("Web login home ");
+    }
+
+    @Test
+    public void MobileLoginHomeLoan(){
+        System.out.println("Mobile login home ");
+
+
+    }
+
+    @Test
+    public void LoginApiHomeLoan(){
+        // rest API automation
+        System.out.println("API login home ");
+
+    }
+}
